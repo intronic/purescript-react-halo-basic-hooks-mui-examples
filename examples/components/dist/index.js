70589f2372bd4d0bf5c9d31d32c00d8515430d45
Michael Pheasant <mike@pheasant.co.nz>
2021-11-20 16:03:38 +1000

    components example

---
+++ examples/components/dist/index.js
@@ -0,0 +1,26 @@
+if (process.env.NODE_ENV === 'production') {
+  // 'parcel build' will include only this code branch (parcel adds process.env.NODE_ENV)
+  require('./example.js')
+} else {
+  // 'parcel serve' will include only this code branch
+  var Main = require('../../../output/Example.Components.Main');
+
+  function main () {
+    // See for configuration options: https://github.com/purescript/spago#get-started-from-scratch-with-parcel-frontend-projects
+    Main.main();
+  }
+
+  // HMR setup. For more info see: https://parceljs.org/hmr.html
+  if (module.hot) {
+    console.log('Configure HMR');
+    module.hot.accept(function () {
+      console.log('Reloaded, running main again');
+      main();
+    });
+  }
+
+  console.log('Starting app');
+
+  main();
+
+}
