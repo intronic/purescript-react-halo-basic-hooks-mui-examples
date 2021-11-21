5e7c32584365fc7c34f1311a8739f42572c20216
Michael Pheasant <mike@pheasant.co.nz>
2021-11-22 07:12:03 +1000

    simplify

--- examples/basic/dist/index.js
+++ examples/basic/dist/index.js
@@ -1,3 +1,11 @@
+// See: https://v4.mui.com/components/typography/#general
+//   Be careful when using this approach. Make sure your bundler doesn't eager load all the font variations (100/300/400/500/700/900, italic/regular, SVG/woff).
+//   import '@fontsource/roboto';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
 if (process.env.NODE_ENV === 'production') {
   // 'parcel build' will include only this code branch (parcel adds process.env.NODE_ENV)
   require('./example.js')
--- examples/components/dist/fonts.js
+++
@@ -1,7 +0,0 @@
-// See: https://v4.mui.com/components/typography/#general
-//   Be careful when using this approach. Make sure your bundler doesn't eager load all the font variations (100/300/400/500/700/900, italic/regular, SVG/woff).
-//   import '@fontsource/roboto';
-import '@fontsource/roboto/300.css';
-import '@fontsource/roboto/400.css';
-import '@fontsource/roboto/500.css';
-import '@fontsource/roboto/700.css';
